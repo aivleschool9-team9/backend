e968b8f8c7d02b3c7b7eb30823070ea2a6451a86
seongukim <csc920406@gmail.com>
2026-06-11 16:00:40 +0900

    클릭 추적 기능 추가

--- src/main/java/com/aivle/bookapp/controller/SearchController.java
+++ src/main/java/com/aivle/bookapp/controller/SearchController.java
@@ -10,7 +10,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.Time;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
